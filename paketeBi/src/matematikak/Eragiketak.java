06cf0f781ab910aeafc4a5c9aef254a184aaeaa6
1ag3.iorilope <1ag3.iorilope@Enara-p22.tolosaldealh.eus>
2023-11-21 09:35:31 +0100

    modified push

--- paketeBi/src/matematikak/Eragiketak.java
+++ paketeBi/src/matematikak/Eragiketak.java
@@ -1,45 +1,60 @@
-/**
- * 
+/*
+ * 2023(e)ko aza. 21(a)
+ * Ioritz Lopetegi
  */
 package matematikak;
 
+// TODO: Auto-generated Javadoc
 /**
- * 
- * @author Ioritz Lopetegi
- * 
- * 
+ * The Class Eragiketak.
  */
 
 public class Eragiketak {
+	
+	/** The z1. */
 	public static int z1;
+	
+	/** The z2. */
 	public static int z2; 
-	
-	
+
+
+	/**
+	 * Batura.
+	 *
+	 * @param z1 the z1
+	 * @param z2 the z2
+	 * @return the int
+	 */
 	public static int batura(int z1, int z2) {
-		
 		return (z1 +z2);
-		
 	}
-	
+
+	/**
+	 * Potentzia.
+	 *
+	 * @param z1 the z 1
+	 * @param z2 the z 2
+	 * @return the int
+	 */
 	public static int potentzia (int z1, int z2) {
 		int intpotentzia = (int) Math.pow(z1, z2);
 		return intpotentzia;
-		
+
 	}
 }
 
 
 
 
-	
-	
-	
 
 
 
 
 
 
-	
+
+
+
+
 
 
--- paketeBi/src/matematikak/Nagusia.java
+++ paketeBi/src/matematikak/Nagusia.java
@@ -1,29 +1,32 @@
-/**
- * 
+/*
+ * 2023(e)ko aza. 21(a)
+ * Ioritz Lopetegi
  */
 package matematikak;
 
 import java.util.Scanner;
 
+// TODO: Auto-generated Javadoc
 /**
- * 
- * @author Ioritz Lopetegi
+ *  Class Nagusia.
  */
 public class Nagusia {
 
 	/**
-	 * @param args
+	 * The main method.
+	 *
+	 * @param args the arguments
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Sartu zenbaki bat");
 		Eragiketak.z1 = sc.nextInt();
-		
+
 		System.out.println("Sartu  beste zenbaki bat");
 		Eragiketak.z2 = sc.nextInt();
-		
-		
+
+
 		System.out.println(Eragiketak.batura(Eragiketak.z1, Eragiketak.z2));
 		System.out.println(Eragiketak.potentzia(Eragiketak.z1, Eragiketak.z2));
 
