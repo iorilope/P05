a35a42af8abdd606123f3292b819dd7926f2a87b
1ag3.iorilope <1ag3.iorilope@Enara-p22.tolosaldealh.eus>
2023-11-22 09:43:44 +0100

    Last

--- p05_07/src/p05_07/TestData.java
+++ p05_07/src/p05_07/TestData.java
@@ -7,19 +7,20 @@ package p05_07;
  * 
  * @author Ioritz Lopetegi
  */
-public class TestData {
-public static void main(String [] args){
-Data f = new Data();
+public class TestData
+{
+	public static void main(String[] args) {
+		Data f = new Data();
 
-f.setEguna(3);
-f.setHilabetea(10);
-f.setUrtea(1970);
+		f.setEguna(3);
+		f.setHilabetea(10);
+		f.setUrtea(1970);
 
-System.out.println("Eguna=" + f.getEguna());
-System.out.println("Hilabetea=" + f.getHilabetea());
-System.out.println("Urtea=" + f.getUrtea());
+		System.out.println("Eguna=" + f.getEguna());
+		System.out.println("Hilabetea=" + f.getHilabetea());
+		System.out.println("Urtea=" + f.getUrtea());
 
-System.out.println(f);
+		System.out.println(f);
 
-}
+	}
 }
