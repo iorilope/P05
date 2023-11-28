5bbcbc52db5de8089ab93407ee041b456abfe375
1ag3.iorilope <1ag3.iorilope@Enara-p22.tolosaldealh.eus>
2023-11-28 09:11:07 +0100

    last

--- p05_04/src/p05_04/Parametroak.java
+++ p05_04/src/p05_04/Parametroak.java
@@ -8,15 +8,16 @@ package p05_04;
  * @author Ioritz Lopetegi
  */
 public class Parametroak {
+
 	public static void aldatu(int x) {
 		x++;
 	}
-
 	public static void aldatu2(int [] par) {
 		par [0] ++;
 	}
 
 	public static void main (String [] args) {
+
 		int zbk1 = 0;
 		int [] zbki2 = { 0 };
 
@@ -25,7 +26,6 @@ public class Parametroak {
 		aldatu2 (zbki2 );
 		System.out.println ("2. zenbakia:" + zbki2 [0]);
 	}
-
 }
 
 //Emaitza 1 zenbakia 0 da aldagai sinplea baita ,orde zbk2 array batean dagoenez elementua eguneratu egingo da
